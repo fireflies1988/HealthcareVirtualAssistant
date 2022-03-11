d789bec544dcde7024f0028a17be5889681d7bfa
Lưu Tiến Phát <luutienphat@gmail.com>
2022-03-11 23:56:40 +0700

    Remove the global variable that handle arduino, instead that add a function to init it and call when user want to measure heart rate

--- sensor.py
+++ sensor.py
@@ -1,37 +1,47 @@
-import time
-import speech_recognition as sr
 import serial
-import pyttsx3
+import time
+import virtual_assistant
 import asyncio
 
-# Khởi tạo phần giọng nói cho ứng dụng
-ann = pyttsx3.init()  # Khởi tạo dịch vụ pyttsx3
-voices = ann.getProperty('voices')  # Sử dụng gói giọng nói trong pyttsx3
-ann.setProperty('voice', voices[1].id)  # Sử dụng giọng nữ cho gói giọng nói này
 
 # Parameters:
 # port: tên cổng nối với arduino
 # baudrate: tốc độ baud ví dụ như 9600 hoạch 11520
 
-arduino_data = serial.Serial('COM3', 115200)  # Khởi tạo cổng mà kết nối tới Adruino
+
+def init_sensor():
+    arduino_data = None
+    try:
+        arduino_data = serial.Serial('COM3', 115200)
+    except serial.SerialException:
+        arduino_data = None
+        pass
+    except():
+        arduino_data = None
+        pass
+    return arduino_data
 
 
-def read_sensor():  # đọc dữ liệu từ cảm biến
+def read_sensor(arduino_data):  # đọc dữ liệu từ cổng serial
     string_data = ""
     if arduino_data.inWaiting() > 0:
         data_packet = arduino_data.readline()
+        # print(data_packet)
         try:
             string_data = data_packet.decode()  # convert from binary string  to string
-            string_data = string_data.strip('\r\n')  # remove '\r\n' characters
         except UnicodeEncodeError:
             pass
+        except:
+            pass
+        string_data = string_data.strip('\r\n')  # remove '\r\n' characters
 
     elif arduino_data.inWaiting() == 0:  # gonna check if arduino has data yet (0: no data, > 0: has data)
         return ""
+    print(string_data)
     return string_data
 
 
-def switch_sensor(order):  # switch on/off sensor (still in development)
+def switch_sensor(order, arduino_data):  # switch on/off sensor (still in development)
     if arduino_data.inWaiting() > 0:
         if order == 'ON':
             arduino_data.write('On sensor')
@@ -39,7 +49,7 @@ def switch_sensor(order):  # switch on/off sensor (still in development)
             arduino_data.write('Off sensor')
 
 
-def switch_arduino(order):  # bật tắt arduino, input vào là 'ON' hoặc 'OFF'
+def switch_arduino(order, arduino_data):  # bật tắt arduino, input vào là 'ON' hoặc 'OFF'
     if order == 'ON':
         arduino_data.write('On sensor')
         arduino_data.open()
@@ -48,80 +58,56 @@ def switch_arduino(order):  # bật tắt arduino, input vào là 'ON' hoặc 'O
         arduino_data.close()
 
 
-def read_heart_rate():
-    if arduino_data.inWaiting() > 0:
-        read_sensor()
+# def read_heart_rate():
+#     if arduino_data.inWaiting() > 0:
+#         read_sensor()
+#
+#
+# def read_spo2_rate():
+#     if arduino_data.inWaiting() > 0:
+#         my_data = arduino_data.readline()
+#         string_data = my_data.decode('ascii')
+#         print(string_data)
 
 
-def read_spo2_rate():
-    if arduino_data.inWaiting() > 0:
-        my_data = arduino_data.readline()
-        string_data = my_data.decode('ascii')
-        print(string_data)
-
-
-def command():
-    c = sr.Recognizer()  # Khởi tạo nhận diện giọng nói
-    with sr.Microphone() as source:  # Nguồn giọng nói đầu vào lấy từ Microphone
-        # c.pause_threshold = 1  # thời gian chờ là 1s
-        c.adjust_for_ambient_noise(source, duration=1)  # giảm tiếng ồn từ môi trường
-        c.energy_threshold = 1000
-
-        print('Listening...')
-        speak('Listening...')
-
-        audio = c.listen(source, timeout=10)  # the program start listening on
-        voice_data = ""
-
-        try:
-            voice_data = c.recognize_google(audio, language='en')  # giọng nói nhận diện sẽ là tiếng anh
-
-        except sr.UnknownValueError:  # bắt lỗi không nhận diện được giọng nói
-            speak("Sorry, I did not get that.")
-            voice_data = command()
-            pass
-
-        except sr.RequestError:
-            speak("Sorry, my speech service is down.")
-            pass
-
-        return voice_data  # trả về mệnh lệnh của người nói ở dạng text
-
-
-def speak(text):  # máy sẽ đọc chữ đầu vào
-    ann.say(text)  # đọc chữ
-    ann.runAndWait()  # chạy và đợi
-
-
-def measure():
+def measure(arduino_data):
     raw_data = []
     flag = True
-
-    timeout = None
+    timeout = time.time() + 10
 
     while flag:
-        data_from_sensor = read_sensor()
-        if 'No finger' in data_from_sensor:
-            raw_data.clear()
-            timeout = None
-            # print('Please put your finger on the sensor')
-
-        elif data_from_sensor.__len__() == 0:
-            continue
+        data_from_sensor = read_sensor(arduino_data)
+        print(data_from_sensor)
+        # if 'No finger' in data_from_sensor:
+        #     raw_data.clear()
+        #     timeout = None
+        #     # print('Please put your finger on the sensor')
+        #
+        # elif data_from_sensor.__len__() == 0:
+        #     continue
+        # else:
+        #     if timeout is None:
+        #         timeout = time.time() + 15
+        #
+        if time.time() > timeout:
+            flag = False
+        # else:
+        # if count > 10 * 10000000000000000:
+        #     flag = False
         else:
-            if timeout is None:
-                timeout = time.time() + 10
+            # count += 1
+            raw_data.append(data_from_sensor)
 
-            if time.time() > timeout:
-                flag = False
-            else:
-                raw_data.append(data_from_sensor)
-                # print(data_from_sensor)
+        time.sleep(1)
 
     data = raw_data[raw_data.__len__() - 1]
     array = data.split(",")
-    speak("Your heart rate is " + array[array.__len__() - 1])
+    virtual_assistant.speak("Your heart rate is " + array[0] + " bpm")
+    virtual_assistant.speak("and the spo2 is " + array[array.__len__() - 1] + " percent")
 
 
 if __name__ == "__main__":  # chỉ chạy các chức năng có trong file sensor và không chạy trong các file khác
-    measure()
+    arduino = init_sensor()
+    # while True:
+    #     read_sensor(arduino)
+    measure(arduino)
